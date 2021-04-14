21e79410c3b46cc7897d75b77b5812b03f163510
AtticusPham <thaipham7778@gmail.com>
2021-04-14 22:31:03 +0700

    Loading a new track from track list

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let trackArt = document.querySelector(".track-art");
 let trackName = document.querySelector(".track-name");
 let trackArtist = document.querySelector(".track-artist");
 
-let prevBtn = document.querySelector(".prev-track");
+let prevTrack = document.querySelector(".prev-track");
 let playpauseBtn = document.querySelector(".playpause-track");
-let nextBtn = document.querySelector("next-track");
+let nextTrack = document.querySelector("next-track");
 
 let seekSlider = document.querySelector(".seek_slider");
 let volumnSlider = document.querySelector(".volumn_slider");
@@ -43,3 +43,49 @@ let track_list = [
       path: "Shipping_Lanes.mp3",
     },
 ];
+
+function loadTrack(trackIndex) {
+  // clear the previous seek timer
+  clearInterval(updateTimer);
+  resetValue();
+
+  // load a new track
+  currTrack.src = track_list[trackIndex].path;
+  currTrack.load();
+
+  //update details of the track
+  trackArt.style.backgroundImage = "url(" + track_list[trackIndex].image + ")";
+  trackName.textContent = track_list[trackIndex].name;
+  trackArtist = track_list[trackIndex].artist;
+  nowPlaying.textContent = "PLAYING" + (trackIndex + 1) + " OF " + track_list.length;
+
+  //set an interval of 1000 miliseconds
+  // for updating the seek slider
+  updateTimer = setInterval(seekUpdate, 1000);
+
+  // move to the next track if the current finishes playing 
+  // use the ended event
+  currTrack.addEventListener("ended", nextTrack);
+  
+  // apply a random background color
+  random_bg_color();
+}
+function random_bg_color() {
+  // Get a random number between 64 to 256
+  // (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+  
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+  
+  // Set the background to the new color
+  document.body.style.background = bgColor;
+}
+// function to reset all values to their default
+function resetValue() {
+  currTime.textContent = "00:00";
+  totalDuration.textContent = "00:00";
+  seekSlider.value = 0;
+}
